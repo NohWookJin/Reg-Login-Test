bff28f7b08174d30668d1fadb76386f4485610c1
NohWookJin <jinn98@kakao.com>
2023-05-16 16:06:39 +0900

    feat: 회원가입 구현

--- src/containers/Auth/RegisterContainer.js
+++ src/containers/Auth/RegisterContainer.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeField, InitializeForm } from "../../store/auth";
+import { register } from "../../store/auth";
 
 import AuthLayout from "../../Layout/AuthLayout";
+import { useNavigate } from "react-router-dom";
 
 const RegisterContainer = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
   }));
+
   const onSubmit = (e) => {
     e.preventDefault();
+    const { email, nick_name, password } = form;
+    console.log("onSubmit(dispatch)");
+    dispatch(register({ email, nick_name, password }));
   };
+
   const onChange = (e) => {
     const { name, value } = e.target;
     dispatch(
@@ -22,9 +33,21 @@ const RegisterContainer = () => {
       })
     );
   };
+
   useEffect(() => {
     dispatch(InitializeForm("register"));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (auth) {
+      console.log(auth);
+      console.log(1);
+    }
+    if (authError) {
+      console.log(authError);
+      console.log(2);
+    }
+  }, [auth, authError]);
 
   return (
     <AuthLayout
