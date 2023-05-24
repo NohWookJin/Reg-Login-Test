93e18fdfda4b4fa833747c386dfee6c92f4f60ec
NohWookJin <jinn98@kakao.com>
2023-05-24 18:29:31 +0900

    feat: Routing 진행중

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import AuthMain from "./containers/Auth/AuthMain";
 import LoginContainer from "./containers/Auth/LoginContainer";
 import RegisterContainer from "./containers/Auth/RegisterContainer";
+import MainContainer from "./containers/Common/MainContainer";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AuthMain />} />
-      <Route path="/login" element={<LoginContainer />} />
+      <Route path="/home" element={<AuthMain />} />
+      <Route path="/auth/login" element={<LoginContainer />} />
       <Route path="/register" element={<RegisterContainer />} />
+      <Route path="/" element={<MainContainer />} />
     </Routes>
   );
 }
