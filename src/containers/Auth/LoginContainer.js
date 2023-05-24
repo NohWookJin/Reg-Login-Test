d1bde35115600e10191116d34ffe02f37007f19a
NohWookJin <jinn98@kakao.com>
2023-05-24 18:31:57 +0900

    feat: 로그인 컨테이너 함수 작성

--- src/containers/Auth/LoginContainer.js
+++ src/containers/Auth/LoginContainer.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { changeField, InitializeForm } from "../../store/auth";
+import { login } from "../../store/user";
 
 import AuthLayout from "../../Layout/AuthLayout";
 
 const LoginContainer = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+
+  const [error, setError] = useState(null);
+
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
+
   const onSubmit = (e) => {
     e.preventDefault();
+    console.log(form);
+    const { email, password } = form;
+    dispatch(login({ email, password }));
   };
+
   const onChange = (e) => {
     const { name, value } = e.target;
     dispatch(
@@ -22,9 +36,25 @@ const LoginContainer = () => {
       })
     );
   };
+
   useEffect(() => {
     dispatch(InitializeForm("login"));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (auth) {
+    }
+    if (authError) {
+      setError("로그인 실패");
+      return;
+    }
+  }, [auth, authError]);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
 
   return (
     <AuthLayout
@@ -32,6 +62,7 @@ const LoginContainer = () => {
       form={form}
       onSubmit={onSubmit}
       onChange={onChange}
+      error={error}
     />
   );
 };
